62283b640185c6e912801972007726bd0b8f3cd1
meshu-dev <harmeshuppal@gmail.com>
2022-10-31 18:43:54 +0000

    Updated about mobile page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import '../styles/about.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

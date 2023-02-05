50d160036f7db1daf1515228f09b5844c729aebf
meshu-dev <harmeshuppal@gmail.com>
2023-02-05 20:06:49 +0000

    Remove img code

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,6 @@ import Layout from '../components/Layout/Layout';
 import Head from 'next/head';
 import Chip from '@mui/material/Chip';
 import { getAboutProps } from '../lib/static-props';
-import PHPImage from '../components/Image/PHPImage';
-import NodeImage from '../components/Image/NodeImage';
-import BrowserImage from '../components/Image/BrowserImage';
 
 export async function getStaticProps() {
   return {
@@ -24,40 +21,7 @@ const About = ({ about, langList }) => {
           <div>
             <h2>Technologies</h2>
             <div id='about-techstack-list'>
-              { langList }
               <div dangerouslySetInnerHTML={{ __html: langList }} />
-              <div className='about-techstack'>
-                <PHPImage />
-                <div className="about-techstack-items">
-                  <Chip label="Laravel" />
-                  <Chip label="Symfony" />
-                  <Chip label="MySQL" />
-                </div>
-              </div>
-              <div className='about-techstack'>
-                <NodeImage />
-                <div className="about-techstack-items">
-                  <Chip label="Express.js" />
-                  <Chip label="MongoDB" />
-                  <Chip label="11ty" />
-                </div>
-              </div>
-              <div className='about-techstack'>
-                <div className='about-techstack-iconwrapper'>
-                  <BrowserImage />
-                </div>
-                <div className="about-techstack-items">
-                  <Chip label="React" />
-                  <Chip label="Next.js" />
-                  <Chip label="Angular" />
-                  <Chip label="Vue.js" />
-                  <Chip label="Nuxt.js" />
-                  <Chip label="HTML 5" />
-                  <Chip label="CSS 3" />
-                  <Chip label="SASS" />
-                  <Chip label="JavaScript" />
-                </div>
-              </div>
             </div>
           </div>
         </div>

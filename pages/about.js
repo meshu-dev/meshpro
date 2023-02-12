34e6671eafa4e5461c275d19fbac5fac0f190574
meshu-dev <harmeshuppal@gmail.com>
2023-02-12 16:21:35 +0000

    Update tech list

--- pages/about.js
+++ pages/about.js
@@ -12,13 +12,11 @@ const About = ({ about, techList }) => {
     <Layout pageTitle='About'>
       <div id='about'>
         <h1>About</h1>
-        <div id="about-text" dangerouslySetInnerHTML={{ __html: about }} />
+        <div id="about-text"
+          dangerouslySetInnerHTML={{ __html: about }} />
         <h2>Technologies</h2>
-        <div id="about-techstack-list">
-          <div id="about-techstack-msg">Below shows a list of technologies that I have experience 
-          in with both work experience and personal projects.</div>
-          <div dangerouslySetInnerHTML={{ __html: techList }} />
-        </div>
+        <div id="about-techstack-list"
+          dangerouslySetInnerHTML={{ __html: techList }} />
       </div>
     </Layout>
   )

172150f4e85d33e901ae53cb81c3ab3a5ef9fb9c
meshu-dev <harmeshuppal@gmail.com>
2023-02-12 02:12:32 +0000

    Minor update

--- pages/about.js
+++ pages/about.js
@@ -11,21 +11,19 @@ export async function getStaticProps() {
 const About = ({ about, techList }) => {
   return (
     <Layout pageTitle='About'>
-        <Head>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
-        </Head>
-        <div id='about'>
-          <h1>About</h1>
-          <div id="about-text" dangerouslySetInnerHTML={{ __html: about }} />
-          <div>
-            <h2>Technologies</h2>
-            <div id="about-techstack-list">
-              <div id="about-techstack-msg">Below shows a list of technologies that I have experience 
-              in with both work experience and personal projects.</div>
-              <div dangerouslySetInnerHTML={{ __html: techList }} />
-            </div>
-          </div>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+      </Head>
+      <div id='about'>
+        <h1>About</h1>
+        <div id="about-text" dangerouslySetInnerHTML={{ __html: about }} />
+        <h2>Technologies</h2>
+        <div id="about-techstack-list">
+          <div id="about-techstack-msg">Below shows a list of technologies that I have experience 
+          in with both work experience and personal projects.</div>
+          <div dangerouslySetInnerHTML={{ __html: techList }} />
         </div>
+      </div>
     </Layout>
   )
 }

619af0992d7004a7b4137c6f0d3ffbfba314f18c
Mesh <harmeshuppal@gmail.com>
2023-12-18 15:42:18 +0000

    Add logo icon

--- components/Layout/Logo.vue
+++ components/Layout/Logo.vue
@@ -1,14 +1,23 @@
 <template>
-  <NuxtLink to="/">
+  <NuxtLink id="logo" to="/">
+    <font-awesome-icon id="logo-icon" icon="fa-solid fa-code" size="xl" />
     <span id="logo-text" class="p-button p-component p-button-link">Mesh</span>
   </NuxtLink>
 </template>
 
 <style scoped>
+#logo {
+  display: flex;
+  align-items: center;
+}
+#logo-icon {
+  color: #FFF;
+}
 #logo-text {
   font-family: 'Bebas Neue', sans-serif;
   color: #FFF;
   font-size: 40px;
   letter-spacing: 2px;
+  margin-top: 2px;
 }
 </style>
--- plugins/font-awesome.ts
+++ plugins/font-awesome.ts
@@ -1,8 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faGithub, faLinkedin)
+library.add(
+  faCode,
+  faLinkedin,
+  faGithub
+)
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)

90d31a8acc0cf830cad3c37aa0d4ee8b91fdd84b
Mesh <harmeshuppal@gmail.com>
2024-07-17 17:05:26 +0100

    Update project view

--- enums/image.ts
+++ enums/image.ts
@@ -1,4 +1,5 @@
 export enum ImageEnum {
   Intro = 'https://placehold.co/300x300',
-  About = 'https://placehold.co/500x500'
+  About = 'https://placehold.co/500x500',
+  Project = 'https://placehold.co/650x350'
 }
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
-import ProjectTypeSelector from '@/components/Project/ProjectTypeSelector'
-import ProjectList from '@/components/Project/ProjectList'
-import { login } from '@/services/auth'
+import ProjectBlock from '@/components/Project/ProjectBlock'
 import { getProjects } from '@/services/api'
+import type { Project } from '@/types'
 
 let projects: Projects[] | null = await getProjects()
 </script>
 
 <template>
-  <ProjectList v-if="projects" :projects="projects" />
+  <div id="project-list">
+    <ProjectBlock v-for="project in projects" :project="project" />
+  </div>
 </template>
+
+<style scoped>
+#project-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 40px;
+  row-gap: 60px;
+}
+
+@media (max-width: 1440px) {
+  #project-list {
+    justify-content: space-around;
+  }
+}
+</style>

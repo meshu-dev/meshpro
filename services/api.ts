fdc4ed7050167d52e3ee00f4bdab3ac39a7ceb46
Mesh <harmeshuppal@gmail.com>
2024-08-04 22:21:54 +0100

    Remove unused variables

--- services/api.ts
+++ services/api.ts
@@ -1,10 +1,5 @@
 import { toRaw } from 'vue'
 import type { About, ApiResponse, Intro, IntroSite, Project } from '~/types'
-import { getPortfolioApiUrl, getAuthHeader } from '@/utils/api'
-
-//const meshApiUrl: string = process.env.NUXT_PUBLIC_MESH_API_URL || 'http://localhost:8000'
-
-console.log('process.env', process.env)
 
 const getApiUrl = (): string => {
   const runtimeConfig = useRuntimeConfig()
@@ -26,8 +21,6 @@ export const getIntroText = async (token: string = ''): Promise<Intro> => {
     )
   )
   const apiData: ApiResponse = toRaw(data.value) as ApiResponse
-
-  console.log('apiData', toRaw(data))
 
   return {
     line1: apiData.data.line1,

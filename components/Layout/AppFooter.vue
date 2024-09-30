8692d9c9a8607279ec59d54f9cf4de2d1555381a
Mesh <harmeshuppal@gmail.com>
2024-09-30 02:24:07 +0100

    Update site width after checking Macbook Pro 13.6 inch screen

--- components/Layout/AppFooter.vue
+++ components/Layout/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="py-12 text-xl border-t border-solid border-neutral-600">
-    <div id="footer-content" class="flex flex-row items-baseline justify-between gap-0 mx-auto px-10 max-w-[1440px] max-md:flex-col max-md:items-center max-md:gap-5">
+    <div id="footer-content" class="flex flex-row items-baseline justify-between gap-0 mx-auto px-10 max-w-[1280px] max-md:flex-col max-md:items-center max-md:gap-5">
       <span id="footer-copyright" class="pt-1">Copyright &copy; Mesh Pro. All rights reserved</span>
       <div>
         <span>Built with Laravel &amp; Nuxt.js</span>
--- components/Project/ProjectBlock.vue
+++ components/Project/ProjectBlock.vue
@@ -19,7 +19,7 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <Card v-if="props.project" class="max-w-[640px] rounded rounded-b-xl max-[1440px]:max-w-[500px] max-[1200px]:max-w-[650px] max-[775px]:max-w-[500px]">
     <template #header>
-      <Image imageClass="w-[650px] rounded rounded-t-xl" :src="props.project.image ?? ImageEnum.Project" alt="Image" />
+      <Image imageClass="w-[550px] rounded rounded-t-xl" :src="props.project.image ?? ImageEnum.Project" alt="Image" />
     </template>
     <template #title>
       {{ props.project.description }}

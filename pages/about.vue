1756e1db954c59787e13ea2b461801b8c2ee9f08
Mesh <contact@devpush.io>
2026-04-15 20:18:04 +0100

    Update about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
 import Image from 'primevue/image'
 import { getAboutData, login } from '@/services/api'
 import type { About, Auth } from '@/types'
-import { ImageEnum } from '@/enums/image';
+import { ImageEnum } from '@/enums/image'
 
 const auth: Ref<Auth | null> = ref(
   await login() || null
@@ -14,15 +14,17 @@ const aboutData: Ref<About | null> = ref(
 </script>
 
 <template>
-  <div v-if="aboutData" class="flex gap-28 max-lg:flex-col max-lg:gap-10">
-    <Image :src="aboutData.image_url || ImageEnum.About" width="1280" alt="Image" />
-    <div>
+  
+  <div v-if="aboutData" class="flex justify-center">
+    <div class="max-w-2xl">
       <h1>About Me</h1>
-      <div v-html="aboutData.text" />
-      <h2>Skills</h2>
-      <ul v-if="aboutData.technologies" class="bullet-point-list">
-        <li v-for="technology in aboutData.technologies" class="bullet-point-item">{{ technology }}</li>
-      </ul>
+      <div class="flex flex-col max-lg:flex-row">
+        <div v-html="aboutData.text" />
+        <h2 class="mt-10">Skills</h2>
+        <ul v-if="aboutData.technologies" class="bullet-point-list">
+          <li v-for="technology in aboutData.technologies" class="bullet-point-item">{{ technology }}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
